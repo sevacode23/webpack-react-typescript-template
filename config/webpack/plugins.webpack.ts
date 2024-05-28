5bb9447d37b5b97aaf645ad04c138e064f96de94
Vsevolod Zoria <vsevolodzoria2@gmail.com>
2024-05-28 03:23:22 +0300

    styling

--- config/webpack/plugins.webpack.ts
+++ config/webpack/plugins.webpack.ts
@@ -3,6 +3,7 @@ import HtmlWebpackPlugin from 'html-webpack-plugin';
 import ESLintPlugin from 'eslint-webpack-plugin';
 import ReactRefreshWebpackPlugin from '@pmmmwh/react-refresh-webpack-plugin';
 import ForkTsCheckerWebpackPlugin from 'fork-ts-checker-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 import { IWebpackParams } from './typings';
 
@@ -12,6 +13,10 @@ export const buildPlugins = (params: IWebpackParams) => {
   const plugins: Configuration['plugins'] = [
     new HtmlWebpackPlugin({ template: paths.html, favicon: paths.favicon }),
     new ESLintPlugin({ extensions: ['ts', 'tsx', 'js', 'jsx'] }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
   ];
 
   if (isDev) {
---
+++ src/modules.d.ts
@@ -0,0 +1,9 @@
+declare module '*.module.scss' {
+  interface IClassNames {
+    [className: string]: string;
+  }
+
+  const classNames: IClassNames;
+
+  export = classNames;
+}
